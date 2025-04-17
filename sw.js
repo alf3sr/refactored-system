d0f3d0a3b4889089d7f77901077e08a6e2a64c44
alf3sr <alfiepaine@hotmail.com>
2025-04-17 04:43:41 +0100

    sw.js

--- sw.js
+++ sw.js
@@ -4,25 +4,24 @@ const CORE = [
   './index.html',
   './manifest.json',
   './sw.js',
-  './icon-192.png',
-  './icon-512.png'
+  './Icon-192.png',
+  './Icon-512.png'
 ];
 
-/* Install – cache core files */
 self.addEventListener('install', e =>
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)))
 );
 
-/* Fetch – respond from cache first */
 self.addEventListener('fetch', e =>
   e.respondWith(caches.match(e.request).then(r => r || fetch(e.request)))
 );
 
-/* Activate – clean old caches */
 self.addEventListener('activate', e =>
   e.waitUntil(
     caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+      Promise.all(
+        keys.filter(k => k !== CACHE).map(k => caches.delete(k))
+      )
     )
   )
 );
